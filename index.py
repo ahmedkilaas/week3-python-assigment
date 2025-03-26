9baf320252a8eb73e2e2330229b2bcfa725e7916
ahmedkilaas <ahmedhalabse@gmail.com>
2025-03-26 09:45:27 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,10 @@
 def calculate_discount(price, discount_percent):
     if discount_percent >= 20:
         return price * (1 - discount_percent / 100)
-    else:
-        return price
+    return price
 
-original_price = float(input("Enter the original price of the item: "))
-discount_percent = float(input("Enter the discount percentage: "))
+original_price = float(input("Enter the original price ($): "))
+discount_percent = float(input("Enter the discount (%): "))
 
 final_price = calculate_discount(original_price, discount_percent)
-
-print(f"The final price after applying the discount is: {final_price:.2f}")
+print(f"Final Price: ${final_price:.2f}")
